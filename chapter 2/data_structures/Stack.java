567cd039cf5d177c8f459a0f404ad862d85fcd31
Nikhil Narayanan <bleach.ichigo.anime@gmail.com>
2023-08-14 01:45:07 +0530

    add string reverse example

--- chapter 2/data_structures/Stack.java
+++ chapter 2/data_structures/Stack.java
@@ -1,5 +1,20 @@
 package data_structures;
-
-public class Stack {
+import java.util.Optional;
+public class Stack<V> {
+	private LinkedListNode<V> head;
+	public Stack() 
+	{
+		super();
+	}
+	public void push(V item) 
+	{
+		head=new LinkedListNode<V>(item,head);
+	}
+	public Optional<V> pop()
+	{
+		Optional<LinkedListNode<V>> node= Optional.ofNullable(head);
+		head=node.flatMap(LinkedListNode::getNext).orElse(null);
+		return node.map(LinkedListNode::getValue);
+	}
 
 }
---
+++ chapter 2/examples/StringReverse.java
@@ -0,0 +1,31 @@
+package examples;
+import data_structures.*;
+import java.util.Optional;
+public class StringReverse {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");	
+		System.out.println(reverse("Hello World"));
+		System.out.println(reverse("").isEmpty()?"Empty":"Unexpected");
+	}
+	//This method is used to reverse the String
+	public static String reverse(String str) 
+	{
+		StringBuilder result=new StringBuilder();
+		Stack<Character> stck=new Stack<>();
+		for(char c:str.toCharArray()) 
+		{
+			stck.push(c);
+		}
+		Optional<Character> chr=stck.pop();
+		while(chr.isPresent()) 
+		{
+			result.append(chr.get());
+			chr=stck.pop();
+		}
+		return result.toString();
+		
+	}
+	
+}
